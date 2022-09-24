f92f5c0239f0932694ee5d6dd2c5686e3663c433
yassser2006 <yasser_e2005@hotmail.com>
2022-09-24 15:41:04 +0200

    MLops

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome Moaz MLops'
+    return 'Welcome Moaz MLops.'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
