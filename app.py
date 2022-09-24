2dc67460b1fb1de7a068aff3ea2aaebcf10e0fd7
yassser2006 <yasser_e2005@hotmail.com>
2022-09-24 16:02:30 +0200

    Khaled

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1 style="color:red;">Welcome Nabil MLops.</h1>'
+    return '<h1 style="color:red;">Welcome Khaled MLops.</h1>'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
